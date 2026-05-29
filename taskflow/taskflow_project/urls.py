0f8099c7ad94b3536605c71354b58aa04fee5248
Graham King-Abor <grahamkingaborlm10@gmail.com>
2026-05-29 02:13:17 +0100

    Add login logout and dashboard

--- taskflow/taskflow_project/urls.py
+++ taskflow/taskflow_project/urls.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('tasks.urls')),
     path('', include('users.urls')),
+
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='users/login.html'),
+        name='login'
+    ),
+
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(),
+        name='logout'
+    ),
 ]
--- taskflow/tasks/views.py
+++ taskflow/tasks/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -9,3 +8,12 @@ def home(request):
     """
 
     return render(request, 'home.html')
+
+
+@login_required
+def dashboard(request):
+    """
+    Render the logged-in user's dashboard.
+    """
+
+    return render(request, 'dashboard.html')
